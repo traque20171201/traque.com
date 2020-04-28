c9c1117577a0ea5e7375c1a02660cf4dde5aa2be
traque20171201 <traque20171201@gmail.com>
2020-04-28 23:05:23 +0700

    Update source code for trade

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -5,6 +5,8 @@ class Trade < ApplicationRecord
   enum tradingType: {buy: 1, sell: 2}
   validates :tradingDate, :tradingType, :volume, :price, presence: true
 
+  delegate :code, to: :stock, allow_nil: true
+
   def self.human_enum_name(enum_name, enum_value)
     I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}")
   end
